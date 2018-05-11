7b02bbb1940ff4513ce4498425f26850565d046f
Никита <n.batrakov@itexpert.ru>
2018-05-11 15:03:47 +0300

    Add TypeStyle

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
+import { style } from 'typestyle';
 
-interface AppProps {
+interface TitleState {
     message: string;
 }
 
-export default function () {
-    return <h1>It works!</h1>;
+class Title extends React.Component<TitleState> {
+    private style = style({
+        color: 'red',
+    });
+
+    public render() {
+        return <h1 className={ this.style }>{ this.props.message }</h1>;
+    }
 }
+
+
+
+export default () => {
+    return <Title message="hello"/>;
+};
