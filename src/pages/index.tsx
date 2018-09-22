7d55b290278688cfbefb51f87521b07e641f8d75
Никита <n.batrakov@itexpert.ru>
2018-09-22 22:20:52 +0300

    Add error page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,3 +6,16 @@ export class NotFoundPage extends React.Component {
         return <h1>Not found</h1>;
     }
 }
+
+export class ErrorPage extends React.Component {
+    public render() {
+        return (
+            <div>
+                <h1>Oops...</h1>
+                <p>There was an internal server error.</p>
+                <p>Please refresh the page or contact the support.</p>
+            </div>
+            
+        );
+    }
+}
